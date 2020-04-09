88595b0b0c68dbaab7e9f3994300c8e51861c142
Nikita <mynewacc.create@gmail.com>
2020-04-10 01:00:43 +0300

    feat: implement and test subtask6

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,31 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        fun fib(x: Int): Int = if (x > 2) fib(x - 2) + fib(x - 1) else 1
+
+        val result = mutableListOf<Int>()
+        val fibNumbers = mutableListOf<Int>()
+        var k = 1
+        var nmbr: Int
+        fibNumbers.add(0)
+        do {
+            nmbr = fib(k++)
+            fibNumbers.add(nmbr)
+        } while (nmbr < n)
+
+        var product: Int
+        for (i in 2 until fibNumbers.size) {
+            product = fibNumbers[i - 1] * fibNumbers[i]
+            if (product >= n) {
+                result.add(fibNumbers[i - 1])
+                result.add(fibNumbers[i])
+                if (product == n) result.add(1) else result.add(0)
+                break
+            }
+        }
+        return result.toIntArray()
     }
+
 }
