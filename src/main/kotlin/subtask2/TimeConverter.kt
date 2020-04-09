adc1d0818f58351a4e0c3f18c4a8fe23cdf4c91c
Nikita <mynewacc.create@gmail.com>
2020-04-09 18:23:46 +0300

    feat: implement and test subtask2

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,37 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+
+        var minutes = minute.toInt()
+        val hours = hour.toInt()
+
+        return if (minutes > 59 || minutes < 0) ""
+        else {
+            val numbersTill19 = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+            "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
+            val tens = arrayOf("twenty", "thirty", "forty", "fifty")
+
+            if (minutes < 30)
+                if (minutes < 20)
+                    when (minutes) {
+                        0 -> "${numbersTill19[hours]} o' clock"
+                        15 -> "quarter past ${numbersTill19[hours]}"
+                        else -> "${numbersTill19[minutes]} minutes past ${numbersTill19[hours]}"
+                    }
+                else {
+                    "${tens[0]} ${numbersTill19[minutes % 10]} minutes past ${numbersTill19[hours]}"
+                }
+            else if (minutes == 30) "half past ${numbersTill19[hours]}"
+            else if (minutes == 45) "quarter to ${numbersTill19[hours + 1]}"
+            else {
+                minutes = 60 - minutes
+                if ((minutes / 10) > 0) "${tens[(minutes / 10) - 2]} ${numbersTill19[minutes % 10]} minutes to ${numbersTill19[hours + 1]}"
+                else "${numbersTill19[minutes % 10]} minutes to ${numbersTill19[hours + 1]}"
+            }
+
+        }
+
     }
+
 }
